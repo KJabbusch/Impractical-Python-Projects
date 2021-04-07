fedb228cab191caa4608dd2e8d6194a867a06b48
KJabbusch <kaj12@my.fsu.edu>
2021-04-07 13:54:15 -0700

    function to load dictionary

---
+++ load_dictionary.py
@@ -0,0 +1,24 @@
+"""Load textfile as a list
+
+Arguments: 
+- text file name (and directory path, if needed)
+
+Exceptions:
+- IOError if filename not found
+
+Returns:
+- A list of all words in textfile in lower case
+"""
+import sys
+
+def load(file):
+    """Open textfile & return list of lowercase strings."""
+    try:
+        with open(file) as in_file:
+            loaded_txt = in_file.read().strip().split('\n')
+            loaded_txt = [x.lower() for x in loaded_txt]
+            return loaded_txt
+    except IOError as e:
+        print("{}\nError opening {}. Terminating program.".format(e, file), file=sys.stderr)
+        sys.exit(1)
+
